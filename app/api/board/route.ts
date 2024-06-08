0e332c775acd058b41ea17ebcb97e4f3b9bddcfe
thantzinwinn <thant.zin.winnnn@gmail.com>
2024-06-09 00:35:50 +0630

    update : change the database url for backend

--- app/api/board/route.ts
+++ app/api/board/route.ts
@@ -1,75 +1,74 @@
 import prisma from "@/lib/prisma";
-import { NextRequest,NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { badRequest } from "../utils/api.utils";
 
-
-export const GET = async (req:NextRequest)=>{
-   try {
-      const url = new URL(req.url);
-      const userId = url.searchParams.get("userId");
-      const [createdBoards, assignedBoards] = await Promise.all([
-         prisma.user.findUnique({
-            where:{
-               id:userId!  
+export const GET = async (req: NextRequest) => {
+  try {
+    const url = new URL(req.url);
+    const userId = url.searchParams.get("userId");
+    const [createdBoards, assignedBoards] = await Promise.all([
+      prisma.user.findUnique({
+        where: {
+          id: userId!,
+        },
+        select: {
+          boards: true,
+        },
+      }),
+      prisma?.board.findMany({
+        where: {
+          members: {
+            some: {
+              userId: userId!,
             },
-            select:{
-               boards:true,
-            }
-         }),
-         prisma?.board.findMany({
-            where:{
-               members:{
-                  some:{
-                     userId:userId!
-                  }
-               }
-            },
-         })
-      ])
-      console.log("boards",{ownBoards:createdBoards,assign:assignedBoards})
-      return NextResponse.json({
-         createdBoards,
-         assignedBoards
-      })
-   } catch (error) {
-      badRequest("Error Found in searching Boards",400)
-   }
-}
-
-export const POST = async (req:NextRequest,res:NextResponse)=>{
-   try {
-    const body = await req.json();
-    const {boardName,userId}  = body;
-    const newBoard = await prisma.board.create({
-      data:{
-         name:boardName,
-         userId
-      }
+          },
+        },
+      }),
+    ]);
+    // console.log("boards",{ownBoards:createdBoards,assign:assignedBoards})
+    return NextResponse.json({
+      createdBoards,
+      assignedBoards,
     });
-    await prisma.member.create({
-      data:{
-         boardId:newBoard.id,
-         userId,
-         isAdmin:true
-      }
-    })
-    return NextResponse.json(newBoard)
-   } catch (error) {   
-    return badRequest("Creating board error",400)
-   }
+  } catch (error) {
+    badRequest("Error Found in searching Boards", 400);
+  }
 };
 
-export const DELETE = async(req:NextRequest)=>{
-   try {
-      const url = new URL(req.url);
-      const boardId = url.searchParams.get("boardId");
-      const deletedBoard = await prisma.board.delete({
-         where:{
-            id:boardId!
-         }
-      })
-      return NextResponse.json(deletedBoard)
-   } catch (error) {
-      return badRequest("Error deleting board",400)
-   }
-}
+export const POST = async (req: NextRequest, res: NextResponse) => {
+  try {
+    const body = await req.json();
+    const { boardName, userId } = body;
+    const newBoard = await prisma.board.create({
+      data: {
+        name: boardName,
+        userId,
+      },
+    });
+    await prisma.member.create({
+      data: {
+        boardId: newBoard.id,
+        userId,
+        isAdmin: true,
+      },
+    });
+    return NextResponse.json(newBoard);
+  } catch (error) {
+    return badRequest("Creating board error", 400);
+  }
+};
+
+export const DELETE = async (req: NextRequest) => {
+  try {
+    const url = new URL(req.url);
+    const boardId = url.searchParams.get("boardId");
+    const deletedBoard = await prisma.board.delete({
+      where: {
+        id: boardId!,
+      },
+    });
+    return NextResponse.json(deletedBoard);
+  } catch (error) {
+    return badRequest("Error deleting board", 400);
+  }
+};
